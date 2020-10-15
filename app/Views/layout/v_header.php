28f5a9f8465b0db53f0b1c38822dc14b5cf2b487
imelia <58154068+imelia@users.noreply.github.com>
2020-10-15 09:18:28 +0700

    Pull lagi ayo

--- app/Views/layout/v_header.php
+++ app/Views/layout/v_header.php
@@ -2,10 +2,10 @@
 <header class="masthead">
     <div class="banner">
       <div class="intro-text">
-        <div class="intro-lead-in">Welcome To Smart City Probolinggo!</div>
-        <div class="intro-heading text-uppercase">It's Nice To Meet You</div>
-        <a class="btn btn-primary btn-xl text-uppercase js-scroll-trigger" href="#services">Tell Me More</a>
-          <video width="1470" height="1200" margin-top: "-50%" autoplay loop muted>
+        <div class="intro-lead-in">Welcome to Portal Smart City Probolinggo!</div>
+        <div class="intro-heading text-uppercase">It's Nice to Meet You</div>
+        <a class="btn btn-primary btn-xl text-uppercase js-scroll-trigger" style="margin-top: 4%" href="#services">Masuk Ke Website Utama</a>
+          <video width="1470" height="1200" autoplay loop muted>
             <source src="/assetsfrontend/img/prob.mp4">
           </video>
          
