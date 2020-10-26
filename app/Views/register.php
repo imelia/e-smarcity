be908b42e92ce07c3845152830873c0f4cba48cc
imelia <58154068+imelia@users.noreply.github.com>
2020-10-26 08:56:43 +0700

    Perbaikan Form Registrasi

--- app/Views/register.php
+++ app/Views/register.php
@@ -9,155 +9,87 @@
     <title>Register</title>
 
     <style>
-        :root {
-            --input-padding-x: 1.5rem;
-            --input-padding-y: .75rem;
-        }
-
-        body {
-            background: #007bff;
-            background: linear-gradient(to right, #0062E6, #33AEFF);
-        }
-
-        .card-signin {
-            border: 0;
-            border-radius: 1rem;
-            box-shadow: 0 0.5rem 1rem 0 rgba(0, 0, 0, 0.1);
-        }
-
-        .card-signin .card-title {
-            margin-bottom: 2rem;
-            font-weight: 300;
-            font-size: 1.5rem;
-        }
-
-        .card-signin .card-body {
-            padding: 2rem;
-        }
-
-        .form-signin {
-            width: 100%;
-        }
-
-        .form-signin .btn {
-            font-size: 80%;
-            border-radius: 5rem;
-            letter-spacing: .1rem;
-            font-weight: bold;
-            padding: 1rem;
-            transition: all 0.2s;
-        }
-
-        .form-label-group {
-            position: relative;
-            margin-bottom: 1rem;
-        }
-
-        .form-label-group input {
-            height: auto;
-            border-radius: 2rem;
-        }
-
-        .form-label-group>input,
-        .form-label-group>label {
-            padding: var(--input-padding-y) var(--input-padding-x);
-        }
-
-        .form-label-group>label {
-            position: absolute;
-            top: 0;
-            left: 0;
-            display: block;
-            width: 100%;
-            margin-bottom: 0;
-            /* Override default `<label>` margin */
-            line-height: 1.5;
-            color: #495057;
-            border: 1px solid transparent;
-            border-radius: .25rem;
-            transition: all .1s ease-in-out;
-        }
-
-        .form-label-group input::-webkit-input-placeholder {
-            color: transparent;
-        }
-
-        .form-label-group input:-ms-input-placeholder {
-            color: transparent;
-        }
-
-        .form-label-group input::-ms-input-placeholder {
-            color: transparent;
-        }
-
-        .form-label-group input::-moz-placeholder {
-            color: transparent;
-        }
-
-        .form-label-group input::placeholder {
-            color: transparent;
-        }
-
-        .form-label-group input:not(:placeholder-shown) {
-            padding-top: calc(var(--input-padding-y) + var(--input-padding-y) * (2 / 3));
-            padding-bottom: calc(var(--input-padding-y) / 3);
-        }
-
-        .form-label-group input:not(:placeholder-shown)~label {
-            padding-top: calc(var(--input-padding-y) / 3);
-            padding-bottom: calc(var(--input-padding-y) / 3);
-            font-size: 12px;
-            color: #777;
-        }
-        /* Fallback for Edge
-        -------------------------------------------------- */
-
-        @supports (-ms-ime-align: auto) {
-            .form-label-group>label {
-                display: none;
-            }
-            .form-label-group input::-ms-input-placeholder {
-                color: #777;
-            }
-        }
-
-        /* Fallback for IE
-        -------------------------------------------------- */
-
-        @media all and (-ms-high-contrast: none),
-        (-ms-high-contrast: active) {
-            .form-label-group>label {
-                display: none;
-            }
-            .form-label-group input:-ms-input-placeholder {
-                color: #777;
-            }
-        }
+    *{
+        margin:0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+    html{
+        height: 100%;
+    }
+    body{
+        font-family: 'Segoe UI', sans-serif;;
+        font-size: 1rem;
+        line-height: 1.6;
+        height: 100%;
+    }
+    .wrap {
+        width: 100%;
+        height: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        background: #fafafa;
+    }
+    .login-form{
+        width: 350px;
+        margin: 0 auto;
+        border: 1px solid #ddd;
+        padding: 2rem;
+        background: #ffffff;
+    }
+    .form-input{
+        background: #fafafa;
+        border: 1px solid #eeeeee;
+        padding: 12px;
+        width: 100%;
+    }
+    .form-group{
+        margin-bottom: 1rem;
+    }
+    .form-button{
+        background: #69d2e7;
+        border: 1px solid #ddd;
+        color: #ffffff;
+        padding: 10px;
+        width: 100%;
+        text-transform: uppercase;
+    }
+    .form-button:hover{
+        background: #69c8e7;
+    }
+    .form-header{
+        text-align: center;
+        margin-bottom : 2rem;
+    }
+    .form-footer{
+        text-align: center;
+    }
     </style>
   </head>
   <body>
-    <div class="container">
-        <div class="row justify-content-md-center">
- 
-            <div class="col-6">
+  <div class="wrap">
+        <form class="login-form" action="/register/save" method="post">
+            <div class="form-header">
                 <h1>Sign Up</h1>
                 <?php if(isset($validation)):?>
                     <div class="alert alert-danger"><?= $validation->listErrors() ?></div>
                 <?php endif;?>
-                <form action="/register/save" method="post">
-                    <div class="mb-3">
+                </div>
+                   <!--Email Input-->
+                    <div class="form-group">
                         <label for="InputForName" class="form-label">Nama</label>
                         <input type="text" name="name" class="form-control" id="InputForName" value="<?= set_value('name') ?>">
                     </div>
-                    <div class="mb-3">
+                    <div class="form-group">
                         <label for="InputForEmail" class="form-label">Email</label>
                         <input type="email" name="email" class="form-control" id="InputForEmail" value="<?= set_value('email') ?>">
                     </div>
-                    <div class="mb-3">
+                    <div class="form-group">
                         <label for="InputForPassword" class="form-label">Password</label>
                         <input type="password" name="password" class="form-control" id="InputForPassword">
                     </div>
-                    <div class="mb-3">
+                    <div class="form-group">
                         <label for="InputForConfPassword" class="form-label">Konfirmasi Password</label>
                         <input type="password" name="confpassword" class="form-control" id="InputForConfPassword">
                     </div>
