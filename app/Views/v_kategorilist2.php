4ba4e35f1ff28a629ac845c1cc05fdbf937b0334
imelia <58154068+imelia@users.noreply.github.com>
2020-10-27 14:23:04 +0700

    Update kategorilist

--- app/Views/v_kategorilist2.php
+++ app/Views/v_kategorilist2.php
@@ -219,6 +219,25 @@
 
       </div>
       <!-- End of Main Content -->
+      <!-- Logout Modal-->
+  <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel"></h5>
+          <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">×</span>
+          </button>
+        </div>
+        <div class="modal-body">Apakah anda yakin ingin logout ?</div>
+        <div class="modal-footer">
+          <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
+          <a class="btn btn-primary" href="<?= base_url('login/logout'); ?>" >Logout</a>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!--End Modal Logout-->
   <!-- Bootstrap core JavaScript-->
   <script src="<?php echo base_url() ?>/assets/vendor/jquery/jquery.min.js"></script>
   <script src="<?php echo base_url() ?>/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
