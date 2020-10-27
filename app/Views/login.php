68ff0bef1f1ccb12697842af9ebbe621c9abdfa2
imelia <58154068+imelia@users.noreply.github.com>
2020-10-27 13:42:34 +0700

    Update login

--- app/Views/login.php
+++ app/Views/login.php
@@ -12,7 +12,7 @@
   <title>Admin - Login</title>
 
  <!-- Custom fonts for this template-->
- <link href="<?php echo base_url() ?>/assets/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  <link href="<?php echo base_url() ?>/assets/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
   <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
   <!-- Custom styles for this template-->
@@ -71,7 +71,7 @@
   </div>
 
   <!-- Bootstrap core JavaScript-->
-  <script src="<?php echo base_url() ?>/assets/vendor/jquery/jquery.min.js"></script> href=
+  <script src="<?php echo base_url() ?>/assets/vendor/jquery/jquery.min.js"></script> 
   <script src="<?php echo base_url() ?>/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
   <!-- Core plugin JavaScript-->
